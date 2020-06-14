50eb745af12d333555612d2a71ffce27762f8734
andrepgsilva <andrepgsilva@outlook.com>
2020-06-14 17:00:29 +0100

    feat: Add category model and tests

---
+++ app/Models/Traits/IsOrderable.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+
+trait IsOrderable
+{
+    public function scopeOrdered(Builder $buider, $direction = 'asc') {
+        $buider->orderBy('order', $direction);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        Category::create([
+            'name' => 'Technology',
+            'slug' => 'tech',
+            'order' => 1,
+        ]);
+
+        Category::create([
+            'name' => 'Computers',
+            'slug' => 'computers',
+            'parent_id' => 1,
+            'order' => 2,
+        ]);
     }
 }
