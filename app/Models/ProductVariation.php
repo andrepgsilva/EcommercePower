4fc42ce67bf63b13b66d5f155197e6dec6ba588b
andrepgsilva <andrepgsilva@outlook.com>
2020-06-30 21:37:02 +0100

    feat: Add product variation types with tests

--- app/Models/ProductVariation.php
+++ app/Models/ProductVariation.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\ProductVariationType;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductVariation extends Model
 {
-    //
+    public function type()
+    {
+        return $this->hasOne(ProductVariationType::class, 'id', 'product_variation_type_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
--- database/factories/ProductVariationFactory.php
+++ database/factories/ProductVariationFactory.php
@@ -5,10 +5,12 @@
 use App\Models\Product;
 use Faker\Generator as Faker;
 use App\Models\ProductVariation;
+use App\Models\ProductVariationType;
 
 $factory->define(ProductVariation::class, function (Faker $faker) {
     return [
         'product_id' => factory(Product::class)->create()->id,
-        'name' => $faker->unique()->name
+        'name' => $faker->unique()->name,
+        'product_variation_type_id' => factory(ProductVariationType::class)->create()->id,
     ];
 });
