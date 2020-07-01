1c0a88a35a4e2dfb228a50634d32cc1e99dfa77e
andrepgsilva <andrepgsilva@outlook.com>
2020-07-01 22:12:57 +0100

    feat: Add product prices formatter & tests

--- tests/Unit/Products/ProductsTest.php
+++ tests/Unit/Products/ProductsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Products;
 
+use App\Cart\Money;
 use App\Models\Category;
 use Tests\TestCase;
 use App\Models\Product;
@@ -36,5 +37,21 @@ class ProductsTest extends TestCase
         );
 
         $this->assertInstanceOf(ProductVariation::class, $product->variations->first());
+    }
+
+    public function test_it_returns_a_money_instance_for_the_price()
+    {
+        $product = factory(Product::class)->create();
+
+        $this->assertInstanceOf(Money::class, $product->price);
+    }
+
+    public function test_it_returns_a_formatted_price()
+    {
+        $product = factory(Product::class)->create([
+            'price' => 1000
+        ]);
+
+        $this->assertEquals($product->formattedPrice, '£10.00');
     }
 }
